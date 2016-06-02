86ab681907be0c7faf7fb745426e53f612372a4d
Owen Hughes <okayhughes@gmail.com>
2016-06-01 23:58:23 -0400

    Adds more FZ support

--- depth_driver/src/main.cpp
+++ depth_driver/src/main.cpp
@@ -22,14 +22,15 @@ int main(int argc, char**argv) {
 	
 	//Variables for initializing FZ api
 	//Gets device info from EnumDevices2
-	FZ_DEVICE_INFO DeviceInfo[MAX_DEVICES];
+	FZ_DEVICE_INFO deviceInfo[MAX_DEVICES];
 	//records return value from pretty much every API operation
 	FZ_Result result;
 	//Device handle for communicating with camera
 	FZ_Device_Handle_t device;
 	
 	//config variables
-	uint16_t mode;
+	//mode may be wrong. Alternative is DE_MODE_640X480_640X480; 
+	uint16_t mode = DE_MODE_640X480_30;
 	uint16_t shutterMs = 20;//2ms
 	uint16_t iFPS = 40;
 
@@ -42,8 +43,35 @@ int main(int argc, char**argv) {
 	FZ_SetLogging(iFlags, NULL, FZLogCallback);
 	//TODO: SET UP LOGGING FOR PRODUCTION
 	
+	//enumerate connected devices
+	int numDevices = MAX_DEVICES;
+	result = FZ_EnumDevices2(deviceInfo, &iNumDevices);
+	if(result!=FZ_Success || numDevices<1){
+		printf("ERROR: Devices not found\n");
+		return 1;
+	}
 
+	//open first device
+	result = FZ_Open(deviceInfo[0].szPath, 0,  &device);
+	if(result != FZ_Success){
+		printf("ERROR: Could not open device\n");
+		return 1;
+	}
 	
+	//note: first NULL could be useful if camera becoes unresponsive.
+	//Returns wheter acknowledged or not;
+	result = FZ_IOCtl(device, CMD_DE_SET_MODE, &mode, sizeof(mode), NULL, NULL, NULL);
+	if(result !=FZ_Success){
+		printf("ERROR: Could not set camera mode\n");
+		return 1;
+	}
+
+	//set shutter
+	result = FZ_IOCtl(device, CMD_DE_SET_SHUTTER, &shutterMs, sizeof(shutterMs), NULL, NULL, NULL);
+	if(result != FZ_Success){
+		printf("ERROR: Could not set shutter speed\n");
+		return 1;
+	}
 
 	
 	//Message format that works with publishing
